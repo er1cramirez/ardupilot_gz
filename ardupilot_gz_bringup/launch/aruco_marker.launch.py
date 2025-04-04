6a305ef7793ec0d8bbd0735573e6bda9dbcb12ad
Eric <eric.delarosa@cimat.mx>
2025-04-04 12:16:19 -0600

    change the movement plugin

---
+++ ardupilot_gz_bringup/launch/aruco_marker.launch.py
@@ -0,0 +1,40 @@
+# aruco_marker.launch.py
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_project_gazebo = get_package_share_directory("ardupilot_gz_gazebo")
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+
+    # Gazebo
+    gz_sim_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={
+            "gz_args": "-v4 -s -r "
+            f'{Path(pkg_project_gazebo) / "worlds" / "aruco_world.sdf"}'
+        }.items(),
+    )
+
+    gz_sim_gui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={"gz_args": "-v4 -g"}.items(),
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time", default_value="true", description="Use simulation time."
+            ),
+            gz_sim_server,
+            gz_sim_gui,
+        ]
+    )
---
+++ ardupilot_gz_bringup/launch/wildthumper_aruco.launch.py
@@ -0,0 +1,52 @@
+# wildthumper_aruco.launch.py
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    """Generate a launch description for a rover with ArUco markers."""
+    pkg_project_bringup = get_package_share_directory("ardupilot_gz_bringup")
+    pkg_project_gazebo = get_package_share_directory("ardupilot_gz_gazebo")
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+
+    # Gazebo
+    gz_sim_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={
+            "gz_args": "-v4 -s -r "
+            f'{Path(pkg_project_gazebo) / "worlds" / "wildthumper_aruco_test.sdf"}'
+        }.items(),
+    )
+
+    gz_sim_gui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={"gz_args": "-v4 -g"}.items(),
+    )
+
+    # RViz
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", f'{Path(pkg_project_bringup) / "rviz" / "wildthumper.rviz"}'],
+        condition=IfCondition(LaunchConfiguration("rviz")),
+    )
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "rviz", default_value="true", description="Open RViz."
+            ),
+            gz_sim_server,
+            gz_sim_gui,
+            rviz,
+        ]
+    )
